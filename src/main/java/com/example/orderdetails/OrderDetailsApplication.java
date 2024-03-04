3bbeb4dec4be09007d65b548c31cf51b8dfc7fd1
szintia <kasziba.cintia@gmail.com>
2024-03-04 15:12:30 +0100

    starter implementation of the declarative client - openfeign

--- src/main/java/com/example/orderdetails/OrderDetailsApplication.java
+++ src/main/java/com/example/orderdetails/OrderDetailsApplication.java
@@ -2,8 +2,10 @@ package com.example.orderdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderDetailsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/orderdetails/model/OrderDetails.java
@@ -0,0 +1,6 @@
+package com.example.orderdetails.model;
+
+public class OrderDetails {
+    private int id;
+    private boolean details;
+}
